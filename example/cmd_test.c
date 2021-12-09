2936e490af98ece1290a474db02ff3f16c582d39
Haru Zheng <haruzheng@realtek.com>
2021-12-09 13:21:55 +0800

    Add cmd test example.

---
+++ example/cmd_test.c
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2016-2021 Davidson Francis <davidsondfgl@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <ws.h>
+
+/**
+ * @dir example/
+ * @brief wsServer examples folder
+ *
+ * @file send_receive.c
+ * @brief Simple send/receiver example.
+ */
+
+/**
+ * @brief Called when a client connects to the server.
+ *
+ * @param fd File Descriptor belonging to the client. The @p fd parameter
+ * is used in order to send messages and retrieve informations
+ * about the client.
+ */
+void onopen(int fd)
+{
+	char *cli;
+	cli = ws_getaddress(fd);
+	printf("Connection opened, client: %d | addr: %s\n", fd, cli);
+	free(cli);
+}
+
+/**
+ * @brief Called when a client disconnects to the server.
+ *
+ * @param fd File Descriptor belonging to the client. The @p fd parameter
+ * is used in order to send messages and retrieve informations
+ * about the client.
+ */
+void onclose(int fd)
+{
+	char *cli;
+	cli = ws_getaddress(fd);
+	printf("Connection closed, client: %d | addr: %s\n", fd, cli);
+	free(cli);
+}
+
+/**
+ * @brief Called when a client connects to the server.
+ *
+ * @param fd File Descriptor belonging to the client. The
+ * @p fd parameter is used in order to send messages and
+ * retrieve informations about the client.
+ *
+ * @param msg Received message, this message can be a text
+ * or binary message.
+ *
+ * @param size Message size (in bytes).
+ *
+ * @param type Message type.
+ */
+void onmessage(int fd, const unsigned char *msg, uint64_t size, int type)
+{
+	char *cli;
+	char ret_msg[4096];
+	FILE *cmd_fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	cli = ws_getaddress(fd);
+	printf("I receive a message: %s (size: %" PRId64 ", type: %d), from: %s/%d\n",
+		msg, size, type, cli, fd);
+	free(cli);
+
+	/**
+	 * Mimicks the same frame type received and re-send it again
+	 *
+	 * Please note that we could just use a ws_sendframe_txt()
+	 * or ws_sendframe_bin() here, but we're just being safe
+	 * and re-sending the very same frame type and content
+	 * again.
+	 */
+	//ws_sendframe(fd, (char *)msg, size, true, type);
+
+	// Run Command
+	bzero(ret_msg, 4096);
+	strncpy(ret_msg, (char *)msg, size);
+	strncat(ret_msg, " 2>&1", 4096);
+	cmd_fp = popen(ret_msg, "r");
+	while ((nread = getline(&line, &len, cmd_fp)) != -1) {
+		*(line + nread - 1) = '\0';
+		ws_sendframe(fd, (char *)line, nread, true, type);
+	}
+	pclose(cmd_fp);
+	free(line);
+}
+
+/**
+ * @brief Main routine.
+ *
+ * @note After invoking @ref ws_socket, this routine never returns,
+ * unless if invoked from a different thread.
+ */
+int main(void)
+{
+	struct ws_events evs;
+	evs.onopen    = &onopen;
+	evs.onclose   = &onclose;
+	evs.onmessage = &onmessage;
+	ws_socket(&evs, 8080, 0); /* Never returns. */
+
+	/*
+	 * If you want to execute code past ws_socket, invoke it like:
+	 *   ws_socket(&evs, 8080, 1)
+	 */
+
+	return (0);
+}
